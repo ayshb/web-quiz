bb83adf0ddb8eacedeee4202245ac01baea24467
Balasubramanian <aybala@usarraybalasub0.us.deloitte.com>
2017-02-24 14:34:50 -0500

    Web Quiz

--- quiz.js
+++ quiz.js
@@ -50,6 +50,47 @@ $(function() {
         }
     }(document, "script", "twitter-wjs");
 
+    var $container = $('#container'),
+        $checkboxes = $('#filters input');
+
+    $container.isotope({
+        itemSelector: '.item'
+    });
+    // get Isotope instance
+    var isotope = $container.data('isotope');
+    // add even classes to every other visible item, in current order
+    function addEvenClasses() {
+        isotope.$filteredAtoms.each(function(i, elem) {
+            $(elem)[(i % 2 ? 'addClass' : 'removeClass')]('even')
+        });
+    }
+
+    $checkboxes.change(function() {
+        var filters = [];
+        // get checked checkboxes values
+        $checkboxes.filter(':checked').each(function() {
+            filters.push(this.value);
+        });
+        // ['.red', '.blue'] -> '.red, .blue'
+        filters = filters.join(', ');
+        $container.isotope({
+            filter: filters
+        });
+        addEvenClasses();
+    });
+
+    $('#shuffle').click(function() {
+        $container.isotope('shuffle');
+        addEvenClasses();
+    });
+
+    $('.red').click(function() {
+        $('#landingScreen').addClass('displayNone');
+        $('#quizScreen').removeClass('displayNone');
+        $('#finalScreen').addClass('displayNone');
+    });
+
+
 });
 
 
